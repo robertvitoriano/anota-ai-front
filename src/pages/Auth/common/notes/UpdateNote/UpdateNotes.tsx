16d36e13e47ed58b4d547d0e089ba297498bca80
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-10-24 17:33:40 -0300

    style update note page

--- src/pages/Auth/common/notes/UpdateNote/UpdateNotes.tsx
+++ src/pages/Auth/common/notes/UpdateNote/UpdateNotes.tsx
@@ -1,11 +1,20 @@
 import {Wrapper,
-NoteContainer} from './styled'
+NoteContainer,
+PageTitle,
+NoteTitleInput,
+NoteBody,
+NoteBodyTextArea
+} from './styled'
  export default function UpdateNotes() {
 
    return (
    <Wrapper>
+     <PageTitle>Let's type a note !</PageTitle>
      <NoteContainer>
-
+       <NoteTitleInput/>
+       <NoteBody>
+          <NoteBodyTextArea/>
+       </NoteBody>
      </NoteContainer>
    </Wrapper>
    );
--- src/pages/Auth/common/notes/UpdateNote/styled.ts
+++ src/pages/Auth/common/notes/UpdateNote/styled.ts
@@ -4,12 +4,13 @@ export const Wrapper = styled.div`
 display: flex;
 align-items: center;
 flex-direction: column;
-justify-content: center;
+justify-content: space-evenly;
 width: 100%;
 height: 100vh;
 `
+export const NoteContainer = styled.div``
 
-export const NoteContainer = styled.div`
+export const NoteBody = styled.div`
 display: flex;
 align-items: center;
 flex-direction: column;
@@ -19,4 +20,28 @@ background-color: white;
 border-radius:1.5rem;
 border:1rem solid #370502;
 
+`
+
+export const PageTitle = styled.h1`
+font-family: 'Calligraffitti', cursive;
+font-weight: bold;
+margin-top: 1.5rem;
+font-size: 2.5rem;
+`
+
+export const NoteTitleInput = styled.input`
+border:solid 1rem  #370502;
+width:30vw;
+height:8vh;
+border-radius:1.5rem;
+margin-bottom: 5px;
+
+`
+export const NoteBodyTextArea = styled.textarea`
+width:100%;
+border: none;
+resize: none;
+height:100%;
+outline: none;
+padding: 1rem;
 `
