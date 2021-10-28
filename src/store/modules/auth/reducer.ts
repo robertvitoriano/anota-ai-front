86fe963004510b974342e5d48df7b63909b41ca4
Robert Vitoriano <robertvitoriano@gmail.com>
2021-10-28 08:13:28 -0300

    fix token assignment

--- src/store/modules/auth/reducer.ts
+++ src/store/modules/auth/reducer.ts
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    token: '',
+    token: localStorage.getItem('token') || null,
   },
   reducers: {
     setToken: (state, action) => {
