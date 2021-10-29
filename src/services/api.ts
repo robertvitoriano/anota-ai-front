08f8179ef60179aef0ef006375b9f768ce822a56
Robert Vitoriano <robertvitoriano@gmail.com>
2021-10-29 08:31:33 -0300

    add default token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+//@ts-ignore
+axios.defaults.headers.common.authorizarion = localStorage.getItem('token');
 
 const api = axios.create({
   baseURL:process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:4000'
