21875a1e76c38d929c8cbc26992d0d7aac3918a2
Robert Vitoriano <robertvitoriano@gmail.com>
2021-11-27 07:47:51 -0300

    improve category creating modal styling

--- src/components/AddCategoryModal/index.tsx
+++ src/components/AddCategoryModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Translucent, Wrapper, Modal, Category, CategoryList, CreateCategoryButton, NewCategoryInput } from "./styled";
 
 export default function AddCategoryModal() {
@@ -7,15 +7,32 @@ export default function AddCategoryModal() {
   const [array, setArray] = useState([1, 2, 3, 5, 4, 5, 5, 5, 5, 5, 55]);
   const [isCreatingCategory, setIsCreatingCategory] = useState(false);
   const [newCategory, setNewCategory] = useState("");
+  const categoryListRef = useRef(null);
+  const newCategoryInputRef = useRef(null);
 
-  const handleCategoryCreation = () => {
-    if(!newCategory) setIsCreatingCategory(!isCreatingCategory)
+  useEffect(() => {
+    if (isCreatingCategory) {
+      //@ts-ignore
+      categoryListRef.current.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+      //@ts-ignore
+      newCategoryInputRef.current.focus()
+    }
+
+  }, [isCreatingCategory])
+
+  const handleCategoryCreation = async () => {
+    //@ts-ignore
+    console.log(categoryListRef.current.scrollHeight)
+    if (!newCategory) {
+      setIsCreatingCategory(!isCreatingCategory)
+    }
   }
 
   const handleNewCategoryInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewCategory(e.target.value)
-
-
   }
 
   return (
@@ -23,16 +40,16 @@ export default function AddCategoryModal() {
       <Wrapper >
         <Translucent />
         <Modal >
-          <CategoryList>
+          <CategoryList ref={categoryListRef}>
             {array.map(item => <Category>
               Hello World
             </Category>)}
             {isCreatingCategory
-              && <Category>
-                <NewCategoryInput value ={newCategory} onChange = {handleNewCategoryInput} />
+              && <Category >
+                <NewCategoryInput value={newCategory} onChange={handleNewCategoryInput} placeholder="Type your new category" ref={newCategoryInputRef} />
               </Category>}
           </CategoryList>
-          {newCategory ? <CreateCategoryButton onClick = {handleCategoryCreation} >&#10003;</CreateCategoryButton>: <CreateCategoryButton onClick = {handleCategoryCreation} >+</CreateCategoryButton>}
+          {newCategory ? <CreateCategoryButton onClick={handleCategoryCreation} >&#10003;</CreateCategoryButton> : <CreateCategoryButton onClick={handleCategoryCreation} >+</CreateCategoryButton>}
         </Modal>
       </Wrapper>
     </>
