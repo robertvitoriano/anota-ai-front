18ce794e4f175c57b9b5b7c88ebfea7eed0d102e
Robert Vitoriano <robertvitoriano@gmail.com>
2022-04-20 22:16:15 -0300

    ANOTAF-1 feat: adiciona botao de recuperação de senha

---
+++ src/pages/Unauth/ForgotPassword/ForgotPassword.tsx
@@ -0,0 +1,169 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+import {
+  FormContainer,
+  Wrapper,
+  Content,
+  PresentationSection,
+  FormSection, Input,
+  SignUpButton,
+  LoginMessage,
+  Divider,
+  ButtonsContainer,
+  ReturnButton,
+  ButtonText,
+  ReturnButtonContainer,
+  ReturnButtonIcon,
+  Label,
+  MobileWrapper
+} from './styles'
+import { Form } from 'antd'
+import backgroundImage from 'assets/login_background.jpg'
+import mobileBackgroundImage from 'assets/mobile_login_background.png'
+import { PhoneBreakPoint, DesktopBreakPoint } from 'components/responsive_utilities'
+import { useDispatch } from 'react-redux'
+import { setIsLoading } from 'store/modules/loading/reducer'
+
+import api from 'services/api'
+import { Copyright } from 'components/Copyright'
+const ForgotPassword = () => {
+
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+
+  const onFinish = async (values: any) => {
+    try {
+      dispatch(setIsLoading(true))
+      await api.patch('/users/begin-password-recovery', values);
+      //@ts-ignore
+      const email = document.getElementsByName('email')[0].value;
+      const emailClient = email.split('@')[1].split('.')[0]
+      dispatch(setIsLoading(false))
+      Swal.fire({
+        title: "Email sent !",
+        text: `An email with a recover password link will be sent to ${email}, relax !`,
+        icon: "success",
+        confirmButtonText: 'Abrir E-mail !'
+      }).then(() => window.open(
+        `https://${emailClient}.com`,
+        '_blank'
+      )).then(() => history.push('/'))
+
+    } catch (error) {
+      dispatch(setIsLoading(false))
+      console.error(error)
+      Swal.fire(
+        'Cancelled',
+        String(error),
+        'error'
+      )
+    }
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <DesktopBreakPoint>
+
+        <Wrapper backgroundImage={backgroundImage}>
+          <Copyright text="Developed by Robert Vitoriano" />
+          <Content>
+            <PresentationSection>
+              {/* <Title level={1}>Let's write Some Notes</Title> */}
+
+            </PresentationSection>
+            <FormSection>
+              <Divider>Recover your password !</Divider>
+              <FormContainer>
+                <LoginMessage >Send recover password email !</LoginMessage>
+                <Form
+                  autoComplete="off"
+                  onFinish={onFinish}
+                  onFinishFailed={onFinishFailed}
+                  initialValues={{ remember: true }}
+
+                >
+                  <Label>E-mail !</Label>
+                  <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your e-mail!' }]}
+                  >
+                    <Input
+                      placeholder="Type your e-mail"
+                      name="email"
+                    />
+                  </Form.Item>
+
+                  <ButtonsContainer>
+                    <Form.Item>
+                      <SignUpButton size={'large'} htmlType="submit">
+                        <ButtonText>Send E-mail !</ButtonText>
+                      </SignUpButton>
+                    </Form.Item>
+                    <Form.Item>
+                      <ReturnButtonContainer>
+                        <ReturnButtonIcon src=""></ReturnButtonIcon>
+                        <ReturnButton to="/login">Return</ReturnButton>
+                      </ReturnButtonContainer>
+                    </Form.Item>
+                  </ButtonsContainer>
+                </Form>
+              </FormContainer>
+            </FormSection>
+          </Content>
+
+
+        </Wrapper>
+      </DesktopBreakPoint>
+      <PhoneBreakPoint>
+        <MobileWrapper backgroundImage={mobileBackgroundImage}>
+          <FormSection>
+            <FormContainer>
+              <LoginMessage >Register yourself !</LoginMessage>
+              <Form
+                autoComplete="off"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                initialValues={{ remember: true }}
+
+              >
+                <Label>E-mail !</Label>
+                <Form.Item
+                  name="email"
+                  rules={[{ required: true, message: 'Please input your e-mail!' }]}
+                >
+                  <Input
+                    placeholder="Type your e-mail"
+                    name="email"
+                  />
+                </Form.Item>
+
+                <ButtonsContainer>
+                  <Form.Item>
+                    <SignUpButton size={'large'} htmlType="submit">
+                      <ButtonText>Send Sign Up E-mail !</ButtonText>
+                    </SignUpButton>
+                  </Form.Item>
+                  <Form.Item>
+                    <ReturnButtonContainer>
+                      <ReturnButtonIcon src=""></ReturnButtonIcon>
+                      <ReturnButton to="/login">Return</ReturnButton>
+                    </ReturnButtonContainer>
+                  </Form.Item>
+                </ButtonsContainer>
+              </Form>
+            </FormContainer>
+          </FormSection>
+        </MobileWrapper>
+      </PhoneBreakPoint>
+    </>
+  )
+}
+
+export default ForgotPassword
---
+++ src/pages/Unauth/ForgotPassword/styles.ts
@@ -0,0 +1,142 @@
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import {
+  ArrowLeftOutlined
+} from '@ant-design/icons';
+
+import { Input as AntDesignInput, Form as AntDesignForm, Button as AntDesignButton,  Divider as AntDesignDivider } from "antd";
+interface Props {
+  backgroundImage: any
+}
+interface FormSectionProps {
+  mobile?: boolean
+}
+
+export const Wrapper = styled.div<Pick<Props, 'backgroundImage'>>`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background-image: url('${(props) => props.backgroundImage}') ;
+width: 100vw;
+height: 100vh;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+`
+export const FormContainer = styled.div`
+
+`
+export const Content = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+width: 100%;
+height: 100vh;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+
+
+`
+export const PresentationSection = styled.div`
+width: 50%;
+height: 100%;
+
+`
+
+export const FormSection = styled.div`
+width: ${({mobile}:FormSectionProps)=>(mobile ? '100%' : '40%')};
+height: 80%;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+/* border: .5rem solid gray;
+border-radius: 0.5rem; */
+margin-right:${({mobile}:FormSectionProps)=>(mobile ? '0%' : '12%')}; 
+position: relative;
+top:10%;
+
+`
+export const Form = styled(AntDesignForm)`
+
+`
+export const Input = styled(AntDesignInput)`
+border: 2px solid gray;
+border-radius: 2rem;
+height:3rem;
+`
+export const PasswordInput = styled(AntDesignInput.Password)`
+border: 2px solid gray;
+border-radius: 2rem;
+height:3rem;
+`
+export const SignUpButton = styled(AntDesignButton)`
+position: relative;
+background-color: #DEDAE8;
+padding-bottom:10px ;
+border-radius: 1rem;
+`
+
+export const LoginMessage = styled.h2`
+font-family: 'Calligraffitti', cursive;
+margin-bottom: 3rem;
+font-weight: 600;
+font-size: 2rem;
+`
+export const ButtonText = styled.h2`
+font-family: 'Calligraffitti', cursive;
+margin-bottom: 4rem;
+font-weight: 600;
+font-size: 1.5rem;
+
+
+`
+export const Divider = styled(AntDesignDivider)`
+position: absolute;
+top:0;
+color:black;
+`
+export const ButtonsContainer = styled.div`
+display: flex;
+width: 100%;
+justify-content: space-evenly;
+align-items: center;
+flex-direction: column;
+`
+
+export const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
+export const ReturnButtonContainer = styled.div``
+
+export const ReturnButtonIcon = styled(ArrowLeftOutlined)``
+
+export const ReturnButton = styled(Link)`
+ color:black;
+ &:hover{
+   text-decoration: underline;
+   color:black;
+ }
+`
+
+export const Label= styled.h2`
+font-family: 'Calligraffitti', cursive;
+font-weight: 600;
+font-size: 1rem;
+`
+export const MobileWrapper = styled.div<Pick<Props, 'backgroundImage'>>`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background-image: url('${(props) => props.backgroundImage}') ;
+width: 100vw;
+height: 100vh;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+`
